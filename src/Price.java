3609e3a2efe3afbdebb2d6e3c7f0ea39e2e4b933
rl-86 <rl86@live.se>
2024-09-06 10:03:20 +0200

    Relocated src folder

---
+++ src/Price.java
@@ -0,0 +1,32 @@
+public class Price {
+
+    private String time;
+    private int priceKwh;
+
+    //Constructor
+    public Price(String time, int priceKwh) {
+        this.time = time;
+        this.priceKwh = priceKwh;
+    }
+
+    public Price() {
+        this.time = "";
+        this.priceKwh = 0;
+    }
+    public String getTime() {
+        return time;
+    }
+    public int getPriceKwh() {
+        return priceKwh;
+    }
+
+    public void setPriceKwh(int priceKwh) {
+        this.priceKwh = priceKwh;
+    }
+
+    @Override
+    public String toString() {
+        return "kl "+time +"\t"+ priceKwh+" öre";
+    }
+
+}
