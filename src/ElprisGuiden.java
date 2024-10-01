2a77716b2e85b29be2a5dad4eb75ce190e01adca
rl-86 <rl86@live.se>
2024-10-01 11:50:42 +0200

    Moved each switch case into separate methods

--- src/ElprisGuiden.java
+++ src/ElprisGuiden.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 public class ElprisGuiden {
 
     //Scanner to read user inputs from keyboard
-    public static Scanner userInput = new Scanner(System.in);
+    public static Scanner sc = new Scanner(System.in);
     public static String goBack = ("\nTryck \"Enter\" för att gå till huvudmenyn");
 
     public static void main(String[] args) {
@@ -51,152 +51,174 @@ public class ElprisGuiden {
             System.out.println("\nAnge menyval + enter:");
 
 
-            choice = userInput.nextLine();
+            choice = sc.nextLine();
 
             switch (choice) {
 
                 case "1":
-                    System.out.println("Inmatning av elpriser");
-                    System.out.println("Ange elpris i hela ören per kWh(Tex: 50, 80 eller 450) för följande klockslag:\n");
-
-                    for (int i = 0; i < price.length; i++) {
-                        boolean validInput = false;
-
-                        while (!validInput) {
-                            System.out.printf("kl "+price[i].getTime()+": ");
-
-                            try{
-                                price[i].setPriceKwh(userInput.nextInt());
-                                validInput = true;
-
-                            } catch(InputMismatchException e){
-                                System.out.println("Ogiltig inmatning! Ange endast siffror i hela ören.");
-                                userInput.nextLine();
-                            }
-                        }
-                    }
-
-                    System.out.println("\nAlla priser har matats in.");
-                    System.out.println(goBack);
-                    userInput.nextLine();
-                break;
+                    inputPrices(price);
+                    break;
 
                 case "2":
-                    //Lowest price
-                    int minPrice = price[0].getPriceKwh();
-                    String minTime = price[0].getTime();
-
-                    for (int i = 1; i < price.length; i++) {
-                        if (price[i].getPriceKwh() < minPrice) {
-                            minPrice = price[i].getPriceKwh();
-                            minTime = price[i].getTime();
-                        }
-                    }
-
-                    //Highest price
-                    int maxPrice = price[0].getPriceKwh();
-                    String maxTime = price[0].getTime();
-
-                    for (int i = 1; i < price.length; i++) {
-                        if (price[i].getPriceKwh() > maxPrice) {
-                            maxPrice = price[i].getPriceKwh();
-                            maxTime = price[i].getTime();
-                        }
-                    }
-
-                    //Average price
-                    int sum = 0;
-                    int average;
-                    for (int i = 0; i < price.length; i++) {
-                        sum += price[i].getPriceKwh();
-                    }
-                    average = sum / price.length;
-
-                    //Print out
-                    System.out.println("Min, Max och Medelvärde:\n");
-                    System.out.println("Lägsta pris: kl " + minTime + " " + minPrice + " öre");
-                    System.out.println("Högsta pris: kl " + maxTime + " " + maxPrice + " öre");
-                    System.out.print("Medelvärde över dygnet: ");
-                    System.out.println(average + " öre");
-                    System.out.println(goBack);
-                    userInput.nextLine();
-                break;
+                    displayMinMaxAvg(price);
+                    break;
 
                 case "3":
-                    //Make a clone of the price list to not affect the other functions after sorting
-                    Price[] clonedPrices = new Price[price.length];
-
-                    for (int i = 0; i < price.length; i++) {
-                        clonedPrices[i] = new Price(price[i].getTime(), price[i].getPriceKwh());
-                    }
-
-                    Arrays.sort(clonedPrices, new Comparator<Price>() {
-                        @Override
-                        public int compare(Price p1, Price p2) {
-                            return Integer.compare(p1.getPriceKwh(), p2.getPriceKwh());
-                        }
-                    });
-
-                    System.out.println("Sorterat på lägsta pris först:");
-
-                    //Print out a sorted price list
-                    for (Price sortedPrices : clonedPrices) {
-                        System.out.println("kl "+sortedPrices.getTime() + ": " + sortedPrices.getPriceKwh() + " öre");
-                    }
-                    System.out.println(goBack);
-                    userInput.nextLine();
-                break;
+                    sortPriceList(price);
+                    break;
 
                 case "4":
-                    // Find the cheapest 4-hour period of the day
-                    int window = 4;
-                    double bestWindowSum = Double.MAX_VALUE;
-                    int currentWindowSum;
+                    bestChargingTime(price);
+                    break;
 
-                    int bestStartIndex = 0;
-                    int index = 0;
-
-                    for (int i = 0; i < price.length; i++) {
-                        currentWindowSum = 0;
-
-                        for (int j = 0; j < window; j++) {
-                            index = (i + j) % price.length;
-                            currentWindowSum += price[index].getPriceKwh();
-                        }
-
-                        if (currentWindowSum < bestWindowSum) {
-                            bestWindowSum = currentWindowSum;
-                            bestStartIndex = i;
-                        }
-                    }
-
-                    int bestEndIndex = (bestStartIndex + window - 1) % price.length;
-
-                    String startTime = price[bestStartIndex].getTime().substring(0, price[bestStartIndex].getTime().length() - 3);
-                    String endTime = price[bestEndIndex].getTime().substring(3);
-
-                    System.out.println("Bästa 4-timmars fönstret att ladda(ex. bilen) på är:\n");
-                    System.out.println("kl "+startTime + "-" + endTime + " med ett snittpris på: " + bestWindowSum / window + " öre per timme");
-                    System.out.println("\nSumma vid 11kW laddning i 4h: " + bestWindowSum * 11 / 100 + " Kronor");
-
-                    System.out.println(goBack);
-                    userInput.nextLine();
-                break;
-
-                //dev option :)
                 case "5":
-                    System.out.println("Följande priser är sparade: ");
-                    for (Price value : price) {
-                        System.out.println(value);
-                    }
-                    System.out.println(goBack);
-                    userInput.nextLine();
-                break;
+                    displayAllPrices(price);
+                    break;
             }
 
         } while (!choice.equalsIgnoreCase("e"));
 
         System.out.println("Avslutar...");
 
+    }
+
+    //Input prices for each hour of the day
+    private static void inputPrices(Price[] price) {
+        System.out.println("Inmatning av elpriser");
+        System.out.println("Ange elpris i hela ören per kWh(Tex: 50, 80 eller 450) för följande klockslag:\n");
+
+        for (int i = 0; i < price.length; i++) {
+            boolean validInput = false;
+
+            while (!validInput) {
+                System.out.printf("kl "+ price[i].getTime()+": ");
+
+                try{
+                    price[i].setPriceKwh(sc.nextInt());
+                    validInput = true;
+
+                } catch(InputMismatchException e){
+                    System.out.println("Ogiltig inmatning! Ange endast siffror i hela ören.");
+                    sc.nextLine();
+                }
+            }
+        }
+
+        System.out.println("\nAlla priser har matats in.");
+        System.out.println(goBack);
+        sc.nextLine();
+    }
+
+    //Display the lowest, highest and average price
+    private static void displayMinMaxAvg(Price[] price) {
+        //Lowest price
+        int minPrice = price[0].getPriceKwh();
+        String minTime = price[0].getTime();
+
+        for (int i = 1; i < price.length; i++) {
+            if (price[i].getPriceKwh() < minPrice) {
+                minPrice = price[i].getPriceKwh();
+                minTime = price[i].getTime();
+            }
+        }
+
+        //Highest price
+        int maxPrice = price[0].getPriceKwh();
+        String maxTime = price[0].getTime();
+
+        for (int i = 1; i < price.length; i++) {
+            if (price[i].getPriceKwh() > maxPrice) {
+                maxPrice = price[i].getPriceKwh();
+                maxTime = price[i].getTime();
+            }
+        }
+
+        //Average price
+        int sum = 0;
+        int average;
+        for (int i = 0; i < price.length; i++) {
+            sum += price[i].getPriceKwh();
+        }
+        average = sum / price.length;
+
+        //Print out
+        System.out.println("Min, Max och Medelvärde:\n");
+        System.out.println("Lägsta pris: kl " + minTime + " " + minPrice + " öre");
+        System.out.println("Högsta pris: kl " + maxTime + " " + maxPrice + " öre");
+        System.out.print("Medelvärde över dygnet: ");
+        System.out.println(average + " öre");
+        System.out.println(goBack);
+        sc.nextLine();
+    }
+
+    //Sorted price list
+    private static void sortPriceList(Price[] price) {
+        Price[] clonedPrices = new Price[price.length];
+
+        for (int i = 0; i < price.length; i++) {
+            clonedPrices[i] = new Price(price[i].getTime(), price[i].getPriceKwh());
+        }
+
+        Arrays.sort(clonedPrices, new Comparator<Price>() {
+            @Override
+            public int compare(Price p1, Price p2) {
+                return Integer.compare(p1.getPriceKwh(), p2.getPriceKwh());
+            }
+        });
+
+        System.out.println("Sorterat på lägsta pris först:");
+
+        //Print out a sorted price list
+        for (Price sortedPrices : clonedPrices) {
+            System.out.println("kl "+sortedPrices.getTime() + ": " + sortedPrices.getPriceKwh() + " öre");
+        }
+        System.out.println(goBack);
+        sc.nextLine();
+    }
+
+    // Find the cheapest 4-hour period of the day
+    private static void bestChargingTime(Price[] price) {
+        int window = 4;
+        double bestWindowSum = Double.MAX_VALUE;
+        int currentWindowSum;
+
+        int bestStartIndex = 0;
+        int index = 0;
+
+        for (int i = 0; i < price.length; i++) {
+            currentWindowSum = 0;
+
+            for (int j = 0; j < window; j++) {
+                index = (i + j) % price.length;
+                currentWindowSum += price[index].getPriceKwh();
+            }
+
+            if (currentWindowSum < bestWindowSum) {
+                bestWindowSum = currentWindowSum;
+                bestStartIndex = i;
+            }
+        }
+
+        int bestEndIndex = (bestStartIndex + window - 1) % price.length;
+
+        String startTime = price[bestStartIndex].getTime().substring(0, price[bestStartIndex].getTime().length() - 3);
+        String endTime = price[bestEndIndex].getTime().substring(3);
+
+        System.out.println("Bästa 4-timmars fönstret att ladda(ex. bilen) på är:\n");
+        System.out.println("kl "+startTime + "-" + endTime + " med ett snittpris på: " + bestWindowSum / window + " öre per timme");
+        System.out.println("\nSumma vid 11kW laddning i 4h: " + bestWindowSum * 11 / 100 + " Kronor");
+
+        System.out.println(goBack);
+        sc.nextLine();
+    }
+
+    //Display all prices
+    private static void displayAllPrices(Price[] price) {
+        System.out.println("Följande priser är sparade: ");
+        for (Price value : price) {
+            System.out.println(value);
+        }
+        System.out.println(goBack);
+        sc.nextLine();
     }
 }
